83c2a27bb976a6c21176aed51b8ec8185e73da91
haroonraja111 <rajaronny111@gmail.com>
2023-01-27 15:23:35 +0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WaitingComponent } from './waiting/waiting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'test' , component:TestComponent},
+  {path:'login' , component:LoginComponent },
+  {path:'signup' , component:SignUpComponent} ,
+  {path:'waiting' , component:WaitingComponent},  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { WaitingComponent } from './waiting/waiting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    LoginComponent,
+    SignUpComponent,
+    WaitingComponent
   ],
   imports: [
     BrowserModule,
